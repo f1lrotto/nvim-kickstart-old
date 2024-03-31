606eb4de189dd88310ee9541847f42900e25c6c4
Filip Hupka <filip.hupka@gmail.com>
2024-03-31 20:20:58 +0200

    Initial commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,14 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'ThePrimeagen/vim-be-good' },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+}
